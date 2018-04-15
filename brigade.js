b86999d5c7a0544e4f58110bb73d08c4fb2ad492
icsimlai <simchandra@yahoo.com>
2018-04-15 09:54:13 +0530

    Testing-1 on 04-15-2018

--- brigade.js
+++ brigade.js
@@ -9,17 +9,21 @@ events.on("push", () => {
 })
 
 events.on("image_push", (e, p) => {
+  console.log(" **** I'm Docker 'image_push' handler")
   var docker = JSON.parse(e.payload)
-
+  // Here's how you get the tag.
+  var version = docker.target.tag || "latest"
+  console.log("Docker Image version: ")
+  console.log(`Image Version: ${version}`)
+  console.log(docker)  // Here's how you get the tag.
+  var version = docker.target.tag || "latest"
+  console.log("Docker Image version: ")
+  console.log(`Image Version: ${version}`)
+  console.log(docker)
   // Currently the only action sent is 'push', but this makes your script
   // safe for the future.
-  if (docker.action != "push") {
+  if (docker.action != "image_push") {
     console.log(`Ignoring Action: ${docker.action}`)
     return
   }
-
-  // Here's how you get the tag.
-  var version = docker.target.tag || "latest"
-  console.log(`Image Version: ${version}`)
-  console.log(docker)
 })
